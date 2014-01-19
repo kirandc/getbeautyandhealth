0c1789bb88768c0d7b67c410b92668744c6526e8
Kiran Chaudhari <kiran@joshsoftware.com>
2014-01-19 19:30:24 +0530

    Initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getbeautyandhealth::Application.config.secret_key_base = '0941a3e40357bf568b545c9c680577d8462089107f4eacf1e9626a968064a682c7bc907fcd81b41c4129d94e1f7e2f27d8893a3d76e235c7e8f8a68568fea44e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Getbeautyandhealth::Application.config.session_store :cookie_store, key: '_getbeautyandhealth_session'
